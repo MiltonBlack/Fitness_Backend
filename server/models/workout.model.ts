8d9146b06a95680c1f83ea28f1a6968a4a917471
MiltonBlack <arziblack2@gmail.com>
2024-03-21 03:47:18 +0100

    update...

--- server/models/workout.model.ts
+++ server/models/workout.model.ts
@@ -3,6 +3,9 @@ import mongoose, {Schema} from "mongoose";
 const workoutSchema = new Schema({
     categoryId: { type: String, },
     workout: { type: String, },
+    reps: { type: String, },
+    sets: { type: String, },
+    tags: { type: String, },
     description: { type: String, },
     duration: { type: String, },
 }, { timestamps: true });
